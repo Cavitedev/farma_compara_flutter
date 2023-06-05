2a36cf939fdba3e1a8c524750a847298c72f0fff
Carlos Víllora Tercero <carlosvillorat@gmail.com>
2023-06-05 16:15:20 +0200

    feat: Recarga y continuación lista
    Ahora muestra objetos indefinidamente y recarga desde 0 #1

--- lib/application/browser/browser_notifier.dart
+++ lib/application/browser/browser_notifier.dart
@@ -16,13 +16,31 @@ class BrowserNotifier extends StateNotifier<BrowserState> {
   }) : super(BrowserState.init());
 
   final IItemRepository repository;
+  bool isLoading = false;
 
   Future<void> loadItems() async {
+    isLoading = true;
     final Either<ItemsFailure, List<Item>> itemsEither = await repository.readItemsPage(this.state.page);
 
     itemsEither.when((left) => null, (right) {
       List<Item> items = right;
       state = state.copyWith(items: [...state.items, ...items], page: state.page + 1);
+      isLoading = true;
     });
+  }
+
+  void nextPageIfNotLoading() {
+    if (!isLoading) {
+      loadItems();
+    }
+  }
+
+  void clear() {
+    state = state.copyWith(
+        items: [],
+        itemsFound: null,
+        page: 0);
+
+    loadItems();
   }
 }
--- lib/presentation/items_browse.dart
+++ lib/presentation/items_browse.dart
@@ -9,30 +9,49 @@ import '../core/constants/app_margin_and_sizes.dart';
 class ItemsBrowse extends ConsumerWidget {
   const ItemsBrowse({Key? key}) : super(key: key);
 
-
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-
-
     final notifier = ref.read(browserNotifierProvider.notifier);
     final actualPage = ref.read(browserNotifierProvider).page;
 
-    if(actualPage == 0){
+    if (actualPage == 0) {
       notifier.loadItems();
     }
 
     return Scaffold(
-        body: CustomScrollView(
-      slivers: [
-        SliverAppBar(
-          title: Text("Farma Compara"),
+        body: RefreshIndicator(
+      semanticsLabel: "Recargar juegos de mesa",
+      triggerMode: RefreshIndicatorTriggerMode.onEdge,
+      onRefresh: () async {
+        ref.read(browserNotifierProvider.notifier).clear();
+      },
+      child: NotificationListener<ScrollUpdateNotification>(
+        onNotification: (ScrollNotification scrollInfo) {
+          return _onScroll(scrollInfo, ref);
+        },
+        child: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              title: Text("Farma Compara"),
+            ),
+            SliverPadding(
+              padding: const EdgeInsets.symmetric(horizontal: listPadding),
+              sliver: ItemsBrowserLoadedGames(),
+            )
+          ],
         ),
-        SliverPadding(
-          padding: const EdgeInsets.symmetric(horizontal: listPadding),
-          sliver: ItemsBrowserLoadedGames(),
-        )
-      ],
+      ),
     ));
+  }
+
+  bool _onScroll(ScrollNotification scrollInfo, WidgetRef ref) {
+    if (scrollInfo.metrics.pixels > scrollInfo.metrics.maxScrollExtent - 300 &&
+        ref.read(browserNotifierProvider).isLoaded) {
+      final browserNotifier = ref.read(browserNotifierProvider.notifier);
+
+      browserNotifier.nextPageIfNotLoading();
+      return true;
+    }
+    return false;
   }
 }
