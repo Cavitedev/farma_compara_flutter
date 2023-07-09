53b211e04e3a128e406b7f666dc877d49901a867
Carlos Víllora Tercero <carlosvillorat@gmail.com>
2023-07-09 21:52:59 +0200

    feat: #12 Ocultar farmacias filtradas de las opciones de compra

--- lib/presentation/core/shared/shop_badge/shop_badges_row.dart
+++ lib/presentation/core/shared/shop_badge/shop_badges_row.dart
@@ -2,8 +2,11 @@ import 'package:farma_compara/domain/items/item.dart';
 import 'package:farma_compara/domain/items/shop_item.dart';
 import 'package:farma_compara/presentation/core/shared/shop_badge/shop_badge.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ShopBadgesRow extends StatelessWidget {
+import '../../../../application/browser/browser_notifier.dart';
+
+class ShopBadgesRow extends ConsumerWidget {
   final Item item;
 
   const ShopBadgesRow({
@@ -12,24 +15,24 @@ class ShopBadgesRow extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
-    final listWebsites = item.orderedWebsiteItemsByPrice();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final shopKeysFilter = ref.watch(browserNotifierProvider).query.shopList.shopNames;
+
+    final listWebsites =
+        item.orderedWebsiteItemsByPrice().where((websiteItem) => shopKeysFilter.contains(websiteItem.key)).toList();
 
     return Container(
-        height: 70,
-        padding: const EdgeInsets.symmetric(vertical: 8.0),
-
-        child: ListView.builder(
-
-            itemBuilder: (context, index) {
-              final MapEntry<String, ShopItem> shopItem = listWebsites[index];
-              return ShopBadge(shopItem: shopItem);
-            },
-            shrinkWrap: true,
-            physics: const BouncingScrollPhysics(),
-            itemCount: listWebsites.length,
-
-            scrollDirection: Axis.horizontal),
-        );
+      height: 70,
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: ListView.builder(
+          itemBuilder: (context, index) {
+            final MapEntry<String, ShopItem> shopItem = listWebsites[index];
+            return ShopBadge(shopItem: shopItem);
+          },
+          shrinkWrap: true,
+          physics: const BouncingScrollPhysics(),
+          itemCount: listWebsites.length,
+          scrollDirection: Axis.horizontal),
+    );
   }
 }
