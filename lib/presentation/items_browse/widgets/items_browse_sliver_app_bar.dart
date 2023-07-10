e164dc7502b088611e57c97bda2a9ef259764f15
Carlos Víllora Tercero <carlosvillorat@gmail.com>
2023-07-10 12:37:41 +0200

    style: #11 Cambiar título de la primera página a búsqueda

--- lib/presentation/items_browse/widgets/items_browse_sliver_app_bar.dart
+++ lib/presentation/items_browse/widgets/items_browse_sliver_app_bar.dart
@@ -4,7 +4,6 @@ import 'package:routemaster/routemaster.dart';
 
 import '../../../application/browser/browser_notifier.dart';
 import '../../../application/browser/browser_state.dart';
-import '../../../core/constants/app_constants.dart';
 import '../../../domain/items/i_items_browse_query.dart';
 import '../../core/shared/cart_button.dart';
 import '../../core/widgets/format_widgets/outlined_text_field.dart';
@@ -48,8 +47,9 @@ class _ItemsBrowseSliverAppBarState extends ConsumerState<ItemsBrowseSliverAppBa
         onTap: () {
           _changeIsSearchingState(true);
         },
-        child: Text(textController.text.isEmpty ? appName : textController.text),
+        child: Text(textController.text.isEmpty ? "Búsqueda" : textController.text),
       ),
+
       floating: true,
       actions: [
         IconButton(
